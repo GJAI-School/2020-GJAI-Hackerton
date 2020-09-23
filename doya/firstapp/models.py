a417fd5ca228f945b2a10e7f9254f6bca3949e2e
YoonHwan Lee <lyh940830@gmail.com>
2020-09-24 01:59:02 +0900

    update

--- doya/firstapp/models.py
+++ doya/firstapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class voluteers(models.Model):
+    pass
+
+class best_volunteers(models.Model):
+    pass
+
+class volunteer_lst(models.Model):
+    pass
+
+class StudentPost(models.Model):
+    # intro = models.TextField()
+    # writer = models.ForeignKey(
+    #     AiStudents, related_name='post', on_delete=models.SET_NULL, null=True)
+    pass
