3b3b79ab59c02b8e4759d99b645118ff5040e507
YoonHwan Lee <lyh940830@gmail.com>
2020-09-23 22:00:36 +0900

    update

--- doya/doya/urls.py
+++ doya/doya/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('signin', views.signin, name='signin'),
+    path('login', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
