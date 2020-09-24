817b39b3440011e5f2573f7ad5362f681b2cba60
YoonHwan Lee <lyh940830@gmail.com>
2020-09-24 21:15:21 +0900

    update

--- doya/firstapp/views.py
+++ doya/firstapp/views.py
@@ -59,5 +59,22 @@ def page(request):
     return render(request, 'page.html')
 
 
+area_lst = {'Seoul' : '서울특별시', 'Incheon' : '인천광역시', 'Gyeonggi-do' : '경기도', 'Gangwon-do' : '강원도',
+            'Chungcheongnam-do' : '충청남도', 'Sejong' : '세종특별자치시', 'Daejeon' : '대전광역시', 'Chungcheongbuk-do' : '충청북도',
+            'Gyeongsangbuk-do' : '경상북도', 'Gyeongsangnam-do' : '경상남도', 'Daegu' : '대구광역시', 'Jeollabuk-do' : '전라북도', 
+            'Jeollanam-do' : '전라남도', 'Gwangju' : '광주광역시', 'Ulsan' : '울산광역시', 'Busan' : '부산광역시', 'Jeju' : '제주특별자치시도'}
+
+voluteer_type_lst = {'type_1' : '자연환경보호활동', 'type_2' : '지역사회봉사활동', 'type_3' : '기술지원', 'type_4' : '업무보조', 
+                    'type_5' : '정서지원', 'type_6' : '기타봉사', 'type_7' : '생활지원', 'type_8' : '전문봉사'}
+
+affiliation_lst = {'volunteer' : '자원봉사자', 'agency' : '기관'}
+
+
 def write_page(request):
-    return render(request, 'write_page.html')
+    context = {
+        'area_lst' : area_lst,
+        'voluteer_type_lst' : voluteer_type_lst,
+        'affiliation_lst' : affiliation_lst
+    }
+
+    return render(request, 'write_page.html', context)
